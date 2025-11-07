df1ecb38170f00277307e718bdd25b6d0d2b8acf
BanLowi <lowi40k@gmail.com>
2025-11-07 15:45:53 +0100

    useState working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,8 +9,6 @@ export default function Main({ languages }) {
 
                 <Navbar languages={languages} />
 
-
-
             </div>
 
         </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react"
+
 export default function Navbar({ languages }) {
+
+    const [btnId, setBtnID] = useState(-1)
+
+
 
     return (
         <nav className="navbar navbar-expand">
 
+            {/*             {
+                btnId
+            } */}
+
+
+
             {
                 languages.map(language => (
-                    <button key={language.id}>{language.title}</button>
+                    <button key={language.id} onClick={() => setBtnID(language.id)} className={`${btnId == language.id ? "current" : ""}`}>{language.title}</button>
                 ))
             }
-
-
         </nav>
     )
 }
